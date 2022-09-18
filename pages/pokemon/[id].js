fcbc8b0a332a84aa1bde93999afde0e657ecbbf2
ernest_yedigaryan <ernest_yedigaryan@epam.com>
2022-09-18 19:44:07 +0300

    revert to static site gen

--- pages/pokemon/[id].js
+++ pages/pokemon/[id].js
@@ -4,21 +4,21 @@ import Head from "next/head";
 import Link from "next/link";
 import styles from "../../styles/Details.module.css";
 
-// export async function getStaticPaths() {
-//     const resp = await fetch(
-//         "https://jherr-pokemon.s3.us-west-1.amazonaws.com/index.json"
-//     );
-//     const pokemon = await resp.json();
-//
-//     return {
-//         paths: pokemon.map((pokemon) => ({
-//             params: { id: pokemon.id.toString() },
-//         })),
-//         fallback: false,
-//     };
-// }
+export async function getStaticPaths() {
+    const resp = await fetch(
+        "https://jherr-pokemon.s3.us-west-1.amazonaws.com/index.json"
+    );
+    const pokemon = await resp.json();
 
-export async function getServerSideProps({ params }) {
+    return {
+        paths: pokemon.map((pokemon) => ({
+            params: { id: pokemon.id.toString() },
+        })),
+        fallback: false,
+    };
+}
+
+export async function getStaticProps({ params }) {
     const resp = await fetch(
         `https://jherr-pokemon.s3.us-west-1.amazonaws.com/pokemon/${params.id}.json`
     );
